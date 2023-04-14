edd582b7c948e1bcb9e2b4f6b02f4cb7f51211a7
Pranav Kelkar <pskelkar74@gmail.com>
2023-04-14 05:41:08 +0000

    [Step 1] Working code for SentencePiece training and Word2Vec vectorisation

--- .ipynb_checkpoints/create_corpus-checkpoint.py
+++ .ipynb_checkpoints/create_corpus-checkpoint.py
@@ -4,12 +4,38 @@ from Gutenburg books data used in LibriSpeech
 """
 
 import os
+import argparse
 from tqdm import tqdm
+from re import split
+from textwrap import wrap
 
-VERBOSE = True
-CORPUS_FILENAME = os.getcwd() + "/data/gutenberg_no_spaces.txt"
+parser = argparse.ArgumentParser(description="Create the text corpus")
+
+parser.add_argument("--verbose", action="store_true", default=True,
+                    help="verbose mode")
+parser.add_argument("--output",
+                    help="output filepath")
+parser.add_argument("--spaces", action="store_true", default=False,
+                    help="spaces or not")
+parser.add_argument("--new_lines", action="store_true", default=False,
+                    help="new lines between books or not")
+# parser.add_argument("--use_delimiters", default=".|\?|\!|\n",
+parser.add_argument("--use_delimiters", default=".?!\n",
+                   help="split sentences with regex delimiter (default=\.|\?|\!|\n)")
+parser.add_argument("--sentence_limit", default=4000,
+                   help="Limit sentence to a particular length")
+parser.add_argument("--n_lines", default=None,
+                   help="Limit number of lines")
+
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+
+    VERBOSE = args.verbose
+
+    CORPUS_FILENAME = os.getcwd() + f"/data/gutenberg.txt"
+    if args.output is not None:
+        CORPUS_FILENAME = args.output
     if os.path.exists(CORPUS_FILENAME):
         os.remove(CORPUS_FILENAME)
     fp = open(CORPUS_FILENAME, 'w+')
@@ -18,33 +44,58 @@ if __name__ == "__main__":
     encodings = ["ascii", "utf-8"]
     max_length = 0
 
+    book_list = []
+
     for encoding in encodings:
         if VERBOSE:
             print(f"Encoding - {encoding}")
 
         filepath = os.getcwd() + f"/data/LibriSpeech/books/{encoding}/"
 
-        book_list = os.listdir(filepath)
-        if VERBOSE:
-            book_list = tqdm(book_list)
+        book_list.extend([filepath + i for i in os.listdir(filepath)])
+        
+    if VERBOSE:
+        book_list = tqdm(book_list)
+        
+    n_lines = 0
 
-        for book_folder in book_list:
-            try:
-                book = os.listdir(filepath + book_folder)[0]
-                book_path = filepath + book_folder + "/" + book
+    for book_folder in book_list:
+        try:
+            book = os.listdir(book_folder)[0]
+            book_path = book_folder + "/" + book
 
-                with open(book_path, "r", encoding=encoding) as book_file:
+            with open(book_path, "r", encoding=encoding) as book_file:
+                if args.n_lines is not None and n_lines <= int(args.n_lines):
                     book_text = book_file.read()
-                    book_letters = "".join(
-                        i.lower() for i in book_text if i.isalpha()
-                    )
-                    if len(book_letters) > max_length:
-                        max_length = len(book_letters)
 
-                    with open(CORPUS_FILENAME, "a") as corpus_file:
-                        corpus_file.write(book_letters)
-                        corpus_file.write("\n")
+                    final_text = ""
+                    sent_len = 0
 
-            except Exception:
-                if VERBOSE:
-                    book_list.set_postfix({"last error": book_folder, "max length": max_length})
+                    for char in book_text:
+                        if char.isalpha() and sent_len <= args.sentence_limit:
+                            final_text += char.lower()
+                            sent_len += 1
+                        elif char in list(args.use_delimiters) or sent_len > args.sentence_limit:
+                            if final_text[-1] != "\n":
+                                n_lines += 1
+                                if args.n_lines is not None and n_lines <= int(args.n_lines):
+                                    final_text += "\n"
+                                else:
+                                    break
+                            sent_len = 0
+
+                    with open(
+                        CORPUS_FILENAME, "a", encoding="utf-8"
+                    ) as corpus_file:
+                            corpus_file.write(final_text.strip() + "\n")
+                        # if args.new_lines:
+                        #     corpus_file.write("\n")
+
+        except Exception:
+            if VERBOSE:
+                book_list.set_postfix({"last error": book_folder})
+
+    if VERBOSE:
+        print(f"Created file {CORPUS_FILENAME}")
+
+    # print("Max length :", max_length)
--- create_corpus.py
+++ create_corpus.py
@@ -6,6 +6,8 @@ from Gutenburg books data used in LibriSpeech
 import os
 import argparse
 from tqdm import tqdm
+from re import split
+from textwrap import wrap
 
 parser = argparse.ArgumentParser(description="Create the text corpus")
 
@@ -13,12 +15,17 @@ parser.add_argument("--verbose", action="store_true", default=True,
                     help="verbose mode")
 parser.add_argument("--output",
                     help="output filepath")
-parser.add_argument("--n_books", default=None,
-                    help="number of books to include in corpus")
 parser.add_argument("--spaces", action="store_true", default=False,
                     help="spaces or not")
 parser.add_argument("--new_lines", action="store_true", default=False,
                     help="new lines between books or not")
+# parser.add_argument("--use_delimiters", default=".|\?|\!|\n",
+parser.add_argument("--use_delimiters", default=".?!\n",
+                   help="split sentences with regex delimiter (default=\.|\?|\!|\n)")
+parser.add_argument("--sentence_limit", default=4000,
+                   help="Limit sentence to a particular length")
+parser.add_argument("--n_lines", default=None,
+                   help="Limit number of lines")
 
 
 if __name__ == "__main__":
@@ -26,11 +33,7 @@ if __name__ == "__main__":
 
     VERBOSE = args.verbose
 
-    n_books = -1
-    if args.n_books is not None:
-        n_books = int(args.n_books)
-
-    CORPUS_FILENAME = os.getcwd() + f"/data/gns_{n_books}.txt"
+    CORPUS_FILENAME = os.getcwd() + f"/data/gutenberg.txt"
     if args.output is not None:
         CORPUS_FILENAME = args.output
     if os.path.exists(CORPUS_FILENAME):
@@ -50,12 +53,11 @@ if __name__ == "__main__":
         filepath = os.getcwd() + f"/data/LibriSpeech/books/{encoding}/"
 
         book_list.extend([filepath + i for i in os.listdir(filepath)])
-
-    if n_books >= 1:
-        book_list = book_list[:n_books]
-
+        
     if VERBOSE:
         book_list = tqdm(book_list)
+        
+    n_lines = 0
 
     for book_folder in book_list:
         try:
@@ -63,21 +65,31 @@ if __name__ == "__main__":
             book_path = book_folder + "/" + book
 
             with open(book_path, "r", encoding=encoding) as book_file:
-                book_text = book_file.read()
+                if args.n_lines is not None and n_lines <= int(args.n_lines):
+                    book_text = book_file.read()
 
-                join_char = " " if args.spaces else ""
-                book_letters = join_char.join(
-                    i.lower() for i in book_text if i.isalpha()
-                )
-                if len(book_letters) > max_length:
-                    max_length = len(book_letters)
+                    final_text = ""
+                    sent_len = 0
 
-                with open(
-                    CORPUS_FILENAME, "a", encoding="utf-8"
-                ) as corpus_file:
-                    corpus_file.write(book_letters)
-                    if args.new_lines:
-                        corpus_file.write("\n")
+                    for char in book_text:
+                        if char.isalpha() and sent_len <= args.sentence_limit:
+                            final_text += char.lower()
+                            sent_len += 1
+                        elif char in list(args.use_delimiters) or sent_len > args.sentence_limit:
+                            if final_text[-1] != "\n":
+                                n_lines += 1
+                                if args.n_lines is not None and n_lines <= int(args.n_lines):
+                                    final_text += "\n"
+                                else:
+                                    break
+                            sent_len = 0
+
+                    with open(
+                        CORPUS_FILENAME, "a", encoding="utf-8"
+                    ) as corpus_file:
+                            corpus_file.write(final_text.strip() + "\n")
+                        # if args.new_lines:
+                        #     corpus_file.write("\n")
 
         except Exception:
             if VERBOSE:
@@ -86,4 +98,4 @@ if __name__ == "__main__":
     if VERBOSE:
         print(f"Created file {CORPUS_FILENAME}")
 
-    print("Max length :", max_length)
+    # print("Max length :", max_length)
