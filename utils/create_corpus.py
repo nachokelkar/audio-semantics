8ccf2b9f5b983068db82d6124b85be0de6f812ed
Pranav Kelkar <pskelkar74@gmail.com>
2023-05-25 15:00:18 +0200

    Reformatted corpus creation file

--- utils/create_corpus.py
+++ utils/create_corpus.py
@@ -3,34 +3,32 @@ Code to create phoneme sequences from
 Gutenburg books data used in LibriSpeech
 """
 
-import os
-from g2p_en import G2p
 import argparse
-from tqdm import tqdm
-from re import split
-from textwrap import wrap
+import os
 import random
 from multiprocessing import Process
-import concurrent.futures
-from constants import np_variant_ascii, np_variant_utf, p_variant_ascii, p_variant_utf, delimiters
 
+from constants import delimiters, np_variant_ascii, p_variant_ascii
+from g2p_en import G2p
 
 parser = argparse.ArgumentParser(description="Create the text corpus")
 
 parser.add_argument("--verbose", action="store_true", default=True,
                     help="verbose mode")
 parser.add_argument("--sentence_limit", default=4000,
-                   help="Limit sentence to a particular length")
+                    help="Limit sentence to a particular length")
 parser.add_argument("--mode", default="p",
-                   help="phonemes with noise (n) | phonemes (p) | characters (c)")
+                    help="noisy phonemes (n) | phonemes (p) | characters (c)")
 parser.add_argument("--noise_prob", default=0.01,
-                   help="probability of noise (only for mode=n)")
-parser.add_argument("--variant", default="ascii",
-                   help="replace with ascii or utf-8 characters (only for mode=n|p)")
+                    help="probability of noise (only for mode=n)")
 
 
 # Step 1 : Create corpus with only characters
-def parse_as_char(book_text, sentence_limit=4000, *args, **kwargs):
+def parse_as_char(
+        book_text: str,
+        sentence_limit: int = 4000,
+        *args, **kwargs
+):
     """
     Parses the text of a book as character sequences,
     also returns the number of lines in the book
@@ -51,7 +49,12 @@ def parse_as_char(book_text, sentence_limit=4000, *args, **kwargs):
 
 
 # Step 2 : Create corpus with phonemes
-def parse_as_phonemes(book_text, sentence_limit=4000, phoneme_dict=p_variant_ascii, *args, **kwargs):
+def parse_as_phonemes(
+        book_text: str,
+        sentence_limit: int = 4000,
+        phoneme_dict: dict = p_variant_ascii,
+        *args, **kwargs
+):
     """
     Parses the text of a book as phoneme sequences,
     also returns the number of lines in the book
@@ -80,16 +83,23 @@ def parse_as_phonemes(book_text, sentence_limit=4000, phoneme_dict=p_variant_asc
 
 
 # Step 3 : Create corpus with phonemes, and with some variability
-def parse_as_phonemes_noisy(book_text, sentence_limit=4000, phoneme_variant_dict=np_variant_ascii, *args, **kwargs):
+def parse_as_phonemes_noisy(
+        book_text: str,
+        sentence_limit: int = 4000,
+        phoneme_variant_dict: dict = np_variant_ascii,
+        *args, **kwargs
+):
     """
     Parses the text of a book as phoneme sequences by adding some
     variants of each phoneme.
     Also returns the number of lines in the book.
     """
     g2p = kwargs["g2p"]
-    phoneme_chars = [item for sublist in phoneme_variant_dict.keys() for item in sublist]
+    phoneme_chars = [
+        item for sublist in phoneme_variant_dict.keys() for item in sublist
+    ]
     phoneme_chars.append("")
-    
+
     final_text = ""
     parsed_text = ""
     sent_len = 0
@@ -115,15 +125,21 @@ def parse_as_phonemes_noisy(book_text, sentence_limit=4000, phoneme_variant_dict
     return final_text.strip()
 
 
-def parse_book(book_path, book_parser, mode, *args, **kwargs):
+def parse_book(
+        book_path: str,
+        book_parser: callable,
+        mode: str,
+        *args, **kwargs
+):
     """
     Function to parse a single book.
     """
     if mode in ["n", "p"]:
         g2p = kwargs["g2p"]
-    
+
     encoding = "utf-8" if "utf-8" in book_path.lower() else "ascii"
-    output_filepath = os.getcwd() +f"/data/{mode}_data/" +book_path[book_path.rindex("/") + 1:]
+    output_filepath = os.getcwd() + \
+        f"/data/{mode}_data/" + book_path[book_path.rindex("/") + 1:]
 
     try:
         with open(book_path, "r", encoding=encoding) as book_file:
@@ -135,14 +151,24 @@ def parse_book(book_path, book_parser, mode, *args, **kwargs):
 
             with open(output_filepath, "w", encoding="utf-8") as output_file:
                 output_file.write(final_text.strip() + "\n")
-        
+
         return True
-    
+
     except Exception as e:
         print(f"Error with {output_filepath}: {e}.")
         return False
 
-def parse_all(book_list, n_procs, proc_id, mode="n"):
+
+def parse_all(
+        book_list: str,
+        n_procs: int,
+        proc_id: int,
+        mode="n"
+):
+    """
+    Central parsing function that is passed to a process to take in a list of
+    books and parse each based on a hash.
+    """
     parser_dict = {
         "n": parse_as_phonemes_noisy,
         "p": parse_as_phonemes,
@@ -154,10 +180,21 @@ def parse_all(book_list, n_procs, proc_id, mode="n"):
     for book_path in book_list:
         try:
             if (
-                int(book_path[book_path.rindex("/") + 1:book_path.rindex(".")]) % n_procs == proc_id - 1 and
-                not os.path.isfile(os.getcwd() +f"/data/{mode}_data/" +book_path[book_path.rindex("/") + 1:])
+                int(
+                    book_path[book_path.rindex("/") + 1:book_path.rindex(".")]
+                ) % n_procs == proc_id - 1
+                and
+                not os.path.isfile(
+                    os.getcwd() + f"/data/{mode}_data/" +
+                    book_path[book_path.rindex("/") + 1:]
+                )
             ):
-                parse_book(book_path, book_parser=parser_dict[mode], mode=mode, g2p=g2p)
+                parse_book(
+                    book_path,
+                    book_parser=parser_dict[mode],
+                    mode=mode,
+                    g2p=g2p
+                )
         except ValueError:
             continue
 
@@ -168,33 +205,31 @@ if __name__ == "__main__":
 
     encodings = ["ascii", "utf-8"]
     book_folders = []
-    
+
     # Obtain list of all book directories
     for encoding in encodings:
         filepath = f"data/LibriSpeech/books/{encoding}/"
 
         book_folders.extend([filepath + i for i in os.listdir(filepath)])
-        
+
     book_list = []
     for book_folder in book_folders:
         book = os.listdir(book_folder)[0]
         book_path = book_folder + "/" + book
         book_list.append(book_path)
 
-    # with concurrent.futures.ThreadPoolExecutor() as executor:
-    #     try:
-    #         result = executor.map(parse_book, book_list)
-    #     except KeyboardInterrupt:
-    #         executor.cancel()
     n_procs = 4
-    
+
     processes = [
-        Process(target=parse_all, args=(book_list, n_procs, i+1, args.mode, )) for i in range(n_procs)
+        Process(
+            target=parse_all,
+            args=(book_list, n_procs, i+1, args.mode, )
+        ) for i in range(n_procs)
     ]
-    
+
     for process in processes:
         process.start()
-    
+
     try:
         for process in processes:
             process.join()
